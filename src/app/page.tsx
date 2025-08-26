6db48bbcc5ff4ede5674470ea408daeb8a74d53e
Andrea Galia <andrea.galia@vlc2.com>
2025-08-26 16:35:12 +0200

    Aggiunta completa progetto Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import AboutSection from "@/components/AboutSection";
+import HeroSection from "@/components/HeroSection";
+import ProductsSection from "@/components/ProductSection";
+import FAQSection from "@/components/FAQSection"
+
+export default function HomePage() {
+  return (
+    <>
+    <HeroSection />
+    <ProductsSection />
+    <AboutSection />
+    <FAQSection />
+    </>
+  );
+}
