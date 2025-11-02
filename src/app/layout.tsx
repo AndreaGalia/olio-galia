5a20663f378b67703ffe89617e544325d487ac49
Andrea Galia <andrea.galia@vlc2.com>
2025-11-02 14:43:15 +0100

    add Analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <FloatingCart />
           </CartProvider>
         </LocaleProvider>
+        <Analytics />
       </body>
     </html>
   );
