e3e7a24dbb1940bd00237d7826101e0dc49b81c7
Andrea Galia <andrea.galia@vlc2.com>
2026-02-04 14:43:40 +0100

    fix baseUrl robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next';
 
-const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || process.env.NEXT_PUBLIC_BASE_URL || 'https://oliogalia.it';
+const BASE_URL = (process.env.NEXT_PUBLIC_SITE_URL || process.env.NEXT_PUBLIC_BASE_URL || 'https://oliogalia.com').replace(/\/$/, '');
 
 export default function robots(): MetadataRoute.Robots {
   return {
