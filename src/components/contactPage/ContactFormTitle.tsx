2193fd27879a9802f531e4a537d59f0c0d9a9673
Andrea Galia <andreagalia@outlook.it>
2026-04-14 00:28:04 +0200

    refactor contact page

--- src/components/contactPage/ContactFormTitle.tsx
+++ src/components/contactPage/ContactFormTitle.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { useT } from '@/hooks/useT';
-import { JustifiedWord } from '@/components/ui/JustifiedWord';
 
 export default function ContactFormTitle() {
   const { t } = useT();
   return (
     <div className="mb-0">
       <h2 className="font-serif text-black">
-        <JustifiedWord text={t.contactPage.title} />
+        {t.contactPage.title}
       </h2>
     </div>
   );
